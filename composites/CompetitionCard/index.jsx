9f4ddc8b0da249bd90c8ded24f4ae4219b740b1f
Jargalsaikhan Erdenetsetseg <thejargalsaikhan@gmail.com>
2021-12-11 13:23:21 +0800

    truncate

--- composites/CompetitionCard/index.jsx
+++ composites/CompetitionCard/index.jsx
@@ -14,11 +14,34 @@ const CompetitionCard = ({
   newsLink,
   ...props
 }) => {
+  const truncateText = (text, length) => {
+    const textContent = text
+      .replace(/<h1[^>]*>.*?<\/h1>/g, "")
+      .replace(/<h2[^>]*>.*?<\/h2>/g, "")
+      .replace(/<h3[^>]*>.*?<\/h3>/g, "")
+      .replace(/<h4[^>]*>.*?<\/h4>/g, "")
+      .replace(/<h5[^>]*>.*?<\/h5>/g, "")
+      .replace(/<h6[^>]*>.*?<\/h6>/g, "")
+      .replace(/<ul[^>]*>.*?<\/ul>/g, "")
+      .replace(/<ol[^>]*>.*?<\/ol>/g, "")
+      .replace(/<\/?[^>]+(>|$)/g, "");
+
+    if (!length) {
+      return textContent;
+    }
+
+    if (textContent.length > length) {
+      return `${textContent.substring(0, length)}...`;
+    } else {
+      return textContent;
+    }
+  };
+
   return (
     <Link href={newsLink}>
       <a className={styles.container}>
         <span className={`${styles.tableBodyCol} ${styles.nameCol}`}>
-          {name}
+          {truncateText(name, 100)}
         </span>
         <span className={`${styles.tableBodyCol} ${styles.startCol}`}>
           {startDate}
